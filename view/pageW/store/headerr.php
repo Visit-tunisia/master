bfa0aa69e164aa39d5408c0a2db0d76ef28afe30
medhama <mohamed.fathallah@esprit.tn>
2020-11-28 18:11:39 +0100

    view

---
+++ view/pageW/store/headerr.php
@@ -0,0 +1,97 @@
+<html lang="en">
+  <head>
+    <title>Visit Tunisia</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,700;0,800;1,200;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="shortcut icon" type="image/x-icon" href="images/logo.png">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <div class="wrap">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 d-flex align-items-center">
+            <p class="mb-0 phone pl-md-2">
+            
+          
+              <a href="tel:+953 012 3654 896" class="mr-2"><span class="fa fa-phone mr-1"></span> (+216) 95 00 00 01</a> 
+              <a href="mailto:support@colorlib.com"><span class="fa fa-paper-plane mr-1"></span> VisitTunisia@gmail.com</a>
+            </p>
+          </div>
+          <div class="col-md-6 d-flex justify-content-md-end">
+            <div class="social-media mr-4">
+              <p class="mb-0 d-flex">
+                <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
+              </p>
+            </div>
+            <div class="reg">
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <style type="text/css">
+
+.e{
+height: 80px;
+border-radius: 30%;
+width:80px;
+
+}
+
+
+
+
+</style>
+    
+      <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img  class ="e" src="images/logo.png" > Visit <span>Tunisia </span></a>
+       
+        </div>
+
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Produits</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+                <a class="dropdown-item" href="product.html">liste des produits</a>
+          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+       
+        <li class="nav-item"><a href="lieux.html" class="nav-link">Lieux</a></li>
+        <li class="nav-item"><a href="events.html" class="nav-link">Events</a></li>
+         <li class="nav-item"><a href="panier.html" class="nav-link">Panier</a></li>
+       
+            <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+             <li class="nav-item"><a href="blog.html" class="nav-link">Login</a></li>
+          
+          </ul> 
+        </div>
+      </div>
+    </nav>
+  
+    
+  </body>
+</html>
